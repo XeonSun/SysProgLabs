3f1a2ce6462f0612aa037d704938b61fae42e8d4
Mikhail <mikhail.shornikov@waveaccess.ru>
2021-04-07 20:02:24 +0400

    форма

--- SysProg/Logic/models/File.cs
+++ SysProg/Logic/models/File.cs
@@ -2,7 +2,7 @@
 
 namespace Logic.models
 {
-    class FileDLL
+    class File
     {
         private string _name, _version;
         private DateTime _date;
@@ -11,13 +11,13 @@ namespace Logic.models
         public string Version { get { return _version; } set { _version = value; } }
         public DateTime Date { get { return _date; } set { _date = value; } }
 
-        public FileDLL(string name, string version, DateTime date)
+        public File(string name, string version, DateTime date)
         {
             Name = name;
             Version = version;
             Date = date;
         }
 
-        public FileDLL() { }
+        public File() { }
     }
 }
